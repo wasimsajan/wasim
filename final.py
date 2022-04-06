dbb879357ab278b2843c999f3f15c7b48fa7c3a6
root <root@ip-172-31-93-251.ec2.internal>
2022-04-06 12:17:16 +0000

    added code

---
+++ final.py
@@ -0,0 +1,36 @@
+from github import Github
+import os
+
+token = os.getenv('GITHUB_TOKEN')
+g = Github(token)
+
+def extract_PR(project_full_name):
+    try:
+        current_user = g.get_user()
+        project_full_name = "wasimsajan/devops_training"
+        repo = g.get_repo(project_full_name)
+        PRs_list = repo.get_pulls(state='all')
+        from_email = "was.sajan@gmail.com"
+        to_email = "test@gmail.com"
+        print(f'From Email : # {from_email}')
+        print(f'To  Email : # {to_email}')
+        print(f"Please find below summary of PR for {project_full_name}")
+        for pr in PRs_list:
+            print(f'The PR number is {pr.number}')
+            print(f'The PR id is {pr.id}')
+            print(f'The no of files changed are {pr.changed_files}')
+            print(f'The PR title is {pr.title}\n')
+    except BadCredentialsException as e:
+        print(e.status)
+        print('Bad credentials exception')
+    except UnknownObjectException as e:
+        print(e.status)
+        print('Unknown object exception')
+
+    except GithubException as e:
+        print(e.status)
+        print('General exception')
+
+
+extract_PR('wasimsajan/devops_training')
+
